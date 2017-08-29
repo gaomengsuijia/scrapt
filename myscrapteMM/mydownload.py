2694d7f9012140591686d30915c83f99dd379dc0
thinkpad <thinkpad@126.com>
2017-08-29 21:33:02 +0800

    add 8-29-21

--- myscrapteMM/mydownload.py
+++ myscrapteMM/mydownload.py
@@ -4,55 +4,64 @@ import urllib2
 from mycache import Disk_cache
 from lxml import etree
 import os,sys
+from mydelay import Delay
+
+
+DEFAULT_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+
 class Download(object):
     '''
     下载html
     '''
-    def __init__(self,url):
+    def __init__(self,url,num_retries=None,user_head=DEFAULT_AGENT,cache=None,delay_time = 3):
         self.url = url
+        self.num_retries = num_retries
+        self.user_head = user_head
+        self.cache = cache
+        self.delay = Delay(delay_time)
+
+
+
+    def __call__(self,url):
+        '''
+        ，检查缓存时否可用
+        :param url:
+        :return:
+        '''
+        result = None
+        if self.cache:
+            try:
+                result = self.cache[url]
+            except KeyError as e:
+                pass
+            else:
+                if self.num_retries > 0 and 500<=result['code']<600:
+                    result = None
+
+        if result == None:
+            self.delay.wait()
+            result = self.myload()
+            #将结果保存到缓存中
+            self.cache[url] = result
 
 
     def myload(self):
-        html = urllib2.urlopen(self.url).read()
-        return html
-
-
-    def get_links(self,html_content):
         '''
-        获取url
+        下载html
         :return:
         '''
-        links = etree.HTML(html_content).xpath('//ol[@class="commentlist"]/li/div/div/div[2]/p/img/@src')
-        return links
-
-    def save_img(self,img_link):
-        '''
-        保存图片
-        :return:
-        '''
-        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        img_name = img_link.split('/')[-1]
-        file_foder = BASE_DIR + '/images/'
-        if not os.path.exists(file_foder):
-            os.mkdir(file_foder)
-
-        file_path = file_foder + img_name
         try:
-            img_link = 'http:' + img_link
-            img_stream = urllib2.urlopen(img_link).read()
-        except BaseException:
-            raise BaseException(img_link + 'fail load')
+            html = urllib2.urlopen(self.url).read()
+        except urllib2.URLError as e:
+            print 'download erro',e.reason
+            html = None
+            if self.num_retries > 0:
+                if hasattr(e,'code'):
+                    code = e.code
+                    if 500<e.code<600:
+                        self.myload()
 
-        try:
-            with open(file_path,'wb') as f:
-                f.write(img_stream)
-                print '%s loading sucess'%img_link
-
-        except OSError:
-            raise OSError(file_path +'file not creat')
-
-
-
+        return {'html':html,'code':code}
 
 
 
---
+++ myscrapteMM/mylink_scrapt.py
@@ -0,0 +1,99 @@
+#coding:utf-8
+from lxml import etree
+import os
+import urllib2
+from mydownload import Download
+import re
+import robotparser
+import urlparse
+from mydelay import Delay
+
+DEFAULT_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+
+
+def link_crawler(seed_url,link_re,max_depth=2):
+    '''
+    主运行函数
+    :return:
+    '''
+    urls = [seed_url]
+    # urls_tp = set(urls)
+    urls_dict = {seed_url:0}
+    num_urls = 0
+    rp = get_robots(seed_url)
+
+    ht = Download(seed_url)
+
+    while urls:
+        url = urls.pop()
+        depth = urls_dict[url]
+        if rp.can_fetch(DEFAULT_AGENT,url):
+            links = get_links(ht)
+            for link in links:
+                if re.match(seed_url, link_re):
+                    link = urlparse.urljoin(seed_url, link)
+                    if link not in urls_dict:
+                        urls_dict[link] = depth + 1
+                        urls.append(link)
+
+            num_urls += 1
+            if num_urls > max_depth:
+                break
+        else:
+            print 'not download'
+
+
+
+
+def get_robots(url):
+    '''
+    获取网站的robots.txt
+    :param url:
+    :return:
+    '''
+    rp = robotparser.RobotFileParser(url)
+    rp.set_url(urlparse.urljoin(url + 'robots.txt'))
+    rp.read()
+    return rp
+
+
+
+def get_links(ht):
+
+    '''
+    获取url
+    :return:
+    '''
+    links = etree.HTML(ht).xpath('//ol[@class="commentlist"]/li/div/div/div[2]/p/img/@src')
+    return links
+
+
+def save_img(img_link):
+    '''
+    保存图片
+    :return:
+    '''
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    img_name = img_link.split('/')[-1]
+    file_foder = BASE_DIR + '/images/'
+    if not os.path.exists(file_foder):
+        os.mkdir(file_foder)
+
+    file_path = file_foder + img_name
+    try:
+        img_link = 'http:' + img_link
+        img_stream = urllib2.urlopen(img_link).read()
+    except BaseException:
+        raise BaseException(img_link + 'fail load')
+
+    try:
+        with open(file_path, 'wb') as f:
+            f.write(img_stream)
+            print '%s loading sucess' % img_link
+
+    except OSError:
+        raise OSError(file_path + 'file not creat')
+
+
+if __name__ == "__main__":
+    link_crawler()
